9b457461d4b1cce86da70c5bcbb3a31035d15eb7
Your Name <you@example.com>
2018-08-07 19:53:17 +0200

    working on Search fragment

---
+++ app/src/main/java/com/ale2nico/fillfield/SearchableActivity.java
@@ -0,0 +1,27 @@
+package com.ale2nico.fillfield;
+
+import android.app.Activity;
+import android.app.SearchManager;
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.SearchRecentSuggestions;
+
+public class SearchableActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Intent intent  = getIntent();
+
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            SearchRecentSuggestions suggestions = new SearchRecentSuggestions(this,
+                    MySuggestionProvider.AUTHORITY, MySuggestionProvider.MODE);
+            suggestions.saveRecentQuery(query, null);
+        }
+    }
+
+
+}
