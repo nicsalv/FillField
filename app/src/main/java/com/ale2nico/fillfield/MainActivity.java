7900c829fbb7d628f8bfd9b8eea6c4c8ef1b5601
Your Name <you@example.com>
2018-08-08 15:34:40 +0200

    implmenting search

--- app/src/main/java/com/ale2nico/fillfield/MainActivity.java
+++ app/src/main/java/com/ale2nico/fillfield/MainActivity.java
@@ -177,16 +177,19 @@ public class MainActivity extends AppCompatActivity
         // Inflate the options menu from XML
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.search_menu, menu);
-
-
-        // Get the SearchView and set the searchable configuration
-        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
-        SearchView searchView = (SearchView) menu.findItem(R.id.search).getActionView();
-        // Assumes current activity is the searchable activity
-        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
-        //searchView.setIconifiedByDefault(false); // Do not iconify the widget; expand it by default
-
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.search:
+                onSearchRequested();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
 
--- app/src/main/java/com/ale2nico/fillfield/MySuggestionProvider.java
+++ app/src/main/java/com/ale2nico/fillfield/MySuggestionProvider.java
@@ -3,7 +3,7 @@ package com.ale2nico.fillfield;
 import android.content.SearchRecentSuggestionsProvider;
 
 public class MySuggestionProvider extends SearchRecentSuggestionsProvider {
-    public final static String AUTHORITY = "com.ale2nico.fillfield.MySuggestionProvider";
+    public final static String AUTHORITY = "com.ale2nico.MySuggestionProvider";
     public final static int MODE = DATABASE_MODE_QUERIES;
 
     public MySuggestionProvider() {
