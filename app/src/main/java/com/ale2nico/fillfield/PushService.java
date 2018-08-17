f90818d57ed0fbe9f9f4faa596a7484ae2e5b0f0
Bozzi96 <37588821+Bozzi96@users.noreply.github.com>
2018-08-17 13:48:18 +0200

    Implemented push notification when user reserve one of "My fields" #2 tried to keep service alive (need testing)

---
+++ app/src/main/java/com/ale2nico/fillfield/PushService.java
@@ -0,0 +1,149 @@
+package com.ale2nico.fillfield;
+
+import android.app.AlarmManager;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.os.IBinder;
+import android.os.SystemClock;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.NotificationCompat;
+
+import com.ale2nico.fillfield.models.Field;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class PushService extends Service {
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+    @Override
+    public int onStartCommand(Intent intent,  int flags, int startId) {
+        retrivemsg();
+        return START_STICKY;
+    }
+
+    public void retrivemsg() {
+        // Reference to the agenda table
+        final DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference().child("agenda");
+        databaseReference.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                // A reservation has been added, get field key
+                String key = dataSnapshot.getKey();
+                // Add listener to find out if the reservation was added to one of user's fields
+                databaseReference.getParent().child("fields").child(key)
+                        .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        Field field = dataSnapshot.getValue(Field.class);
+                        String userId = field.getUserId();
+                        // Send notification if reservation was made on one of user's fields
+                        if (userId.equals(FirebaseAuth.getInstance().getUid())) {
+                            sendNotification("ale2nico.FillField", getResources().getString(R.string.new_reservation),
+                                    getResources().getString(R.string.new_reservation_text), getApplicationContext(), MainActivity.class,
+                                    NotificationReceiver.class, 0, 1);
+                        }
+
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+
+        });
+
+
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Intent broadcastIntent = new Intent(".RestartService");
+        sendBroadcast(broadcastIntent);
+    }
+
+    public void sendNotification(String channelId, String contentTitle, String contentText,
+                                 Context packageContext, Class classContext, Class notificationReceiver,
+                                 long delay, Integer notificationId) {
+
+        // [START] Create notification and its settings
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this, channelId)
+                .setSmallIcon(R.mipmap.ic_launcher_round)
+                .setContentTitle(contentTitle)
+                .setContentText(contentText)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setWhen(System.currentTimeMillis() + delay)
+                .setAutoCancel(true);
+        //[END] Create notification and its settings
+
+        // Intent related to current context and class
+        Intent intent = new Intent(packageContext, classContext);
+        intent.putExtra("notificationFragment", "myFieldsFragment");
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+
+
+        // Pending intent for setting notification
+        PendingIntent activityIntent = PendingIntent.getActivity(this, 0,
+                intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        mBuilder.setContentIntent(activityIntent);
+
+        // Build Notification
+        Notification notification = mBuilder.build();
+
+        // Schedule notification with two intents:
+        // notificationIntent for attaching to the BroadcastReceiver
+        Intent notificationIntent = new Intent(packageContext, notificationReceiver);
+        notificationIntent.putExtra(NotificationReceiver.NOTIFICATION_ID, notificationId);
+        notificationIntent.putExtra(NotificationReceiver.NOTIFICATION, notification);
+
+        // PendingIntent and AlarmManager for scheduling the notification at a specific time
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, notificationId, notificationIntent, PendingIntent.FLAG_CANCEL_CURRENT);
+        long futureInMillis = SystemClock.elapsedRealtime() + delay;
+        AlarmManager alarmManager = (AlarmManager) this.getSystemService(Context.ALARM_SERVICE);
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            // Do something for marshmallow and above versions
+            alarmManager.setAndAllowWhileIdle(AlarmManager.ELAPSED_REALTIME_WAKEUP,  futureInMillis, pendingIntent);
+        } else{
+            // do something for phones running an SDK before marshmallow
+            alarmManager.setExact(AlarmManager.ELAPSED_REALTIME_WAKEUP, futureInMillis, pendingIntent);
+        }
+
+    }
+}
