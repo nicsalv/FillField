269b0d44f7fbd67b31fe839f564265562cfe3c71
nicsalv <you@example.com>
2018-08-19 10:05:20 +0200

    Styling fix in ReservationsFragment

--- app/src/main/java/com/ale2nico/fillfield/ReservationsFragment.java
+++ app/src/main/java/com/ale2nico/fillfield/ReservationsFragment.java
@@ -3,14 +3,25 @@ package com.ale2nico.fillfield;
 
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CalendarView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import org.threeten.bp.LocalDate;
 
@@ -28,8 +39,20 @@ public class ReservationsFragment extends Fragment
     // Display reservations for this field only
     private String fieldKey;
 
+    // Display reservations on a specific day
+    RecyclerView mReservationsRecycler;
+
     // Adapter that gets reservations info from the database
     private ReservationsAdapter mReservationsAdapter;
+
+    // Progress Bar displayed during fields loading
+    private ProgressBar loadingProgressBar;
+
+    // Empty view displayed when the reservation list is empty on a selected date
+    private TextView emptyTextView;
+
+    // Reference to the calendar widget in order to be able to hide it
+    private CalendarView calendarView;
 
     public ReservationsFragment() {
         // Required empty public constructor
@@ -63,23 +86,79 @@ public class ReservationsFragment extends Fragment
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_reservations, container, false);
 
+        // Get reference to the progress bar and the empty text view
+        loadingProgressBar = view.findViewById(R.id.reservations_progress_bar);
+        emptyTextView = view.findViewById(R.id.reservations_empty_view);
+
         // Set the listener for the calendar
-        CalendarView calendar = view.findViewById(R.id.reservations_calendar);
-        calendar.setOnDateChangeListener(this);
+        calendarView = view.findViewById(R.id.reservations_calendar);
+        calendarView.setOnDateChangeListener(this);
 
         // Initializing RecyclerView and its layout
-        RecyclerView mReservationsRecycler = view.findViewById(R.id.reservations_list);
+        mReservationsRecycler = view.findViewById(R.id.reservations_list);
         mReservationsRecycler.setLayoutManager(new LinearLayoutManager(view.getContext()));
+        mReservationsRecycler.setVisibility(View.GONE);
 
         // Initialing adapter for the RecyclerView. At first it'll get reservations for today.
-        mReservationsAdapter = new ReservationsAdapter(fieldKey, LocalDate.now().toString());
+        mReservationsAdapter = new ReservationsAdapter(fieldKey, LocalDate.now().toString(),
+                loadingProgressBar, emptyTextView, mReservationsRecycler);
         mReservationsRecycler.setAdapter(mReservationsAdapter);
 
         return view;
     }
 
     @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        // Get field name and set app bar title accordingly
+        FirebaseDatabase.getInstance().getReference().child("fields")
+                .child(fieldKey).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String fieldName = dataSnapshot.child("name").getValue(String.class);
+
+                // Set app bar title
+                getActivity().setTitle(fieldName);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.d("App bar title", "Can't print field name on the app bar.");
+            }
+        });
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        // Inflate menu resource into layout
+        inflater.inflate(R.menu.reservations_menu, menu);
+    }
+
+    public void changeCalendarViewHeight(MenuItem item) {
+        // Show/hide calendarView based on its current state
+
+        if (calendarView.getVisibility() == View.GONE) {
+            // Calendar was hidden. Show it.
+            calendarView.setVisibility(View.VISIBLE);
+            item.setIcon(R.drawable.ic_show_hide_calendar_grey_24dp);
+        }
+        else if (calendarView.getVisibility() == View.VISIBLE) {
+            // Calendar is displayed. Hide it.
+            calendarView.setVisibility(View.GONE);
+            item.setIcon(R.drawable.ic_show_hide_calendar_white_24dp);
+        }
+    }
+
+    @Override
     public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+        // Hide both reservation list and empty view
+        mReservationsRecycler.setVisibility(View.GONE);
+        emptyTextView.setVisibility(View.GONE);
+
+        // Show progress bar
+        loadingProgressBar.setVisibility(View.VISIBLE);
+
         // Cast the selected date as a String
         String selectedDate = MainActivity.getDateFromPicker(year, month, dayOfMonth);
 
